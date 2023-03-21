af47d9fe3070345b570acf0f741185277b6741b9
Simon <simonjudd2@gmail.com>
2023-03-21 00:11:40 -0600

    fix: testnet streaming works

--- backend/src/indexer/constants.py
+++ backend/src/indexer/constants.py
@@ -1,6 +1,6 @@
 from apibara.starknet import felt
 
-deathmachine_testnet = "0x00752a9254b3148fb70e1d00917e62e8f24e2c7c2272899103856eb4c9ff945c"
+deathmachine_testnet = "0x07c521d2f0e60e792c15d41a874f10b4fdcb871299b8d24c8084ec0d9fa8a491"
 boardSummary_testnet = "0x22b803be989158361f3042f192dd0cab0bb034cce1c5e1757483eacf64020c9"
 
 address = felt.from_hex(deathmachine_testnet)
